0d34c9878f99ae71a3afd26f5bf10ce33e872e07
TomOutfit <167419245+TomOutfit@users.noreply.github.com>
2025-07-23 14:02:31 +0700

    Add more UserService test cases and update CI workflow

--- BookStore/BusinessLayer.Tests/UserServiceTests.cs
+++ BookStore/BusinessLayer.Tests/UserServiceTests.cs
@@ -4,6 +4,7 @@ using DataLayer;
 using Moq;
 using Entities;
 using System.Threading.Tasks;
+using System;
 
 public class UserServiceTests
 {
@@ -39,5 +40,20 @@ public class UserServiceTests
 
         // Assert
         Assert.Null(user);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public async Task GetUserByUsernameAsync_ThrowsArgumentException_WhenUsernameIsNullOrEmpty(string username)
+    {
+        // Arrange
+        var mockRepo = new Mock<IUserRepository>();
+        var mockRoleRepo = new Mock<IRoleRepository>();
+        var service = new UserService(mockRepo.Object, mockRoleRepo.Object);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ArgumentException>(() => service.GetUserByUsernameAsync(username));
     }
 } 
